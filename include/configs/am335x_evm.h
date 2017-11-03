e439e4fb855b63c10f9583a23f05f635ff179755
hoahiepnguyen <hiep@olli-ai.com>
2017-11-03 14:31:44 +0700

    flashed FIT image from eMMC into SD-RAM

--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -18,6 +18,20 @@
 
 #include <configs/ti_am335x_common.h>
 
+#ifndef CONFIG_SPL_BUILD
+# define CONFIG_FIT
+# define CONFIG_FIT_VERBOSE
+# define CONFIG_TIMESTAMP
+# define CONFIG_LZO
+# ifdef CONFIG_ENABLE_VBOOT
+#  define CONFIG_OF_CONTROL
+#  define CONFIG_OF_SEPARATE
+#  define CONFIG_DEFAULT_DEVICE_TREE am335x-boneblack
+#  define CONFIG_FIT_SIGNATURE
+#  define CONFIG_RSA
+# endif
+#endif
+
 #define MACH_TYPE_TIAM335EVM		3589	/* Until the next sync */
 #define CONFIG_MACH_TYPE		MACH_TYPE_TIAM335EVM
 #define CONFIG_BOARD_LATE_INIT
@@ -184,10 +198,6 @@
 
 #ifdef CONFIG_BOOT_RESTORE
 #define CONFIG_BOOTCOMMAND \
-	"gpio set 53; " \
-	"i2c mw 0x24 1 0x3e; " \
-	"run findfdt; " \
-	"run mmcboot;" \
 	"gpio clear 56; " \
 	"gpio clear 55; " \
 	"gpio clear 54; " \
@@ -197,17 +207,13 @@
 	"run nandboot;"
 #else
 #define CONFIG_BOOTCOMMAND \
-	"gpio set 53; " \
-	"i2c mw 0x24 1 0x3e; " \
-	"run findfdt; " \
-	"run mmcboot;" \
 	"gpio clear 56; " \
 	"gpio clear 55; " \
 	"gpio clear 54; " \
 	"setenv mmcdev 1; " \
 	"setenv bootpart 1:2; " \
-    "load mmc ${bootpart} 0x82000000 fit;" \
-    "run ramargs; bootz;"
+    "load mmc ${bootpart} 0x82000000 /boot/fit;" \
+    "run ramargs; bootm;"
 #endif
 
 /* NS16550 Configuration */
